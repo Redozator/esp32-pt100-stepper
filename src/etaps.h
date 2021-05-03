75877ba8066c2eb80990a6c0238b44a9a85c965d
Vladimir Redozator <stt_@yahoo.com>
2021-05-03 14:51:52 +0300

    изменение этапа поддержания температуры

--- src/etaps.h
+++ src/etaps.h
@@ -2,6 +2,7 @@
 
 void water_open(int n);
 void water_close(int n);
+double temp_temp; // временное значение температуры. нужно на некоторых этапах
 
 // реагирование на температуру по этапу1
 // ждем когда температура вырастет до 50 градусов и переходим на этап2
@@ -47,6 +48,7 @@ void etap2() {
 void etap3() {
 	if(tempers[POINTS-1]>limit_min) {
 		current_etap = 4;
+		temp_temp = 0;
 		Serial.println("Переход на этап 4");
 		return;
 	}
@@ -67,27 +69,38 @@ void etap3() {
 // поддержание заданной температуры между допустимыми пределами
 void etap4() {
 	double seredina = (limit_min +limit_max) / 2;
-	if(tempers[POINTS-1]<(limit_min-2)) {
+	if(tempers[POINTS-1]<(limit_min-1)) {
 		if(tempers[POINTS-1]<tempers[POINTS-2]) {
 			water_close(1);
-			current_etap = 3;
-			Serial.println("Возвращаемся на этап 3");
+			stepper_pause = 4;
+			if(temp_temp==0 || temp_temp<tempers[POINTS-1]) {
+				temp_temp=tempers[POINTS-1]; // пока не переходим на этап 3, но готовимся
+			} else {
+				if(temp_temp>tempers[POINTS-1]) { // температура продолжает падать, уходим на этап 3
+					temp_temp==0;
+					current_etap = 3;
+					Serial.println("Возвращаемся на этап 3");
+				}
+			}
 			return;
 		}
 	}
 	if(tempers[POINTS-1]>seredina) {
-		if(tempers[POINTS-1]>limit_max || tempers[POINTS-1]>tempers[POINTS-2]) {
+		if(tempers[POINTS-1]>limit_max) {
+			water_open(3); // превышен максимум, открываем воду побольше
+			stepper_pause = 10;
+			return;
+		}
+		if(tempers[POINTS-1]>tempers[POINTS-2]) {
 			water_open(1);
-			stepper_pause = 4;
+			stepper_pause = 5;
 			return;
 		}
 	}
-	if(tempers[POINTS-1]<seredina) {
-		if(tempers[POINTS-1]<tempers[POINTS-2]) {
-			water_close(1);
-			stepper_pause = 4;
-			return;
-		}
+	if(tempers[POINTS-1]<limit_min) {
+		water_close(1);
+		stepper_pause = 5;
+		return;
 	}
 
 }
